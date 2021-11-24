55a2b908fa2704ad4839092549575f654e78662c
Jay Wang <jwang550@my.bcit.ca>
2021-11-24 09:16:46 -0800

    Add working Database (pokemart)

--- PokeMart.js
+++ PokeMart.js
@@ -1,3 +1,4 @@
+// Server-Side
 // Requires
 const express = require("express");
 const session = require("express-session");
@@ -11,15 +12,61 @@ app.use("/css", express.static("public/css"));
 app.use("/images", express.static("public/images"));
 app.use("/html", express.static("public/html"));
 
+// Create new Session, Gives you the "Keycard" that you need to access/open site.
+app.use(session({
+    secret: "",
+    name: "PokeSessionID",
+    resave: false,
+    saveUninitialized: true
+}))
+
+// Retrieve URL at..
 app.get("/", function (req, res) {
 
+    // Serve up this file
     doc = fs.readFileSync("./html/index.html", "utf-8");
     res.send(doc);
 
 });
 
 async function init(){
+
+    // we'll go over promises in COMP 2537, for now know that it allows us
+    // to execute some code in a synchronous manner
+    const mysql = require("mysql2/promise");
+    const connection = await mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "",
+      multipleStatements: true
+    });
+    const createDBAndTables = `CREATE DATABASE IF NOT EXISTS pokemart;
+        use pokemart;
+        CREATE TABLE IF NOT EXISTS user (
+        ID int NOT NULL AUTO_INCREMENT,
+        firstName varchar(30),
+        lastName varchar(30),
+        email varchar(40),
+        password varchar(30),
+        city varchar(50),
+        trainerLevel varchar(10),
+        PRIMARY KEY (ID));`;
+    await connection.query(createDBAndTables);
+
+    // await allows for us to wait for this line to execute ... synchronously
+    // also ... destructuring. There's that term again!
+    const [rows, fields] = await connection.query("SELECT * FROM user");
+    // no records? Let's add a couple - for testing purposes
+    if(rows.length == 0) {
+        // no records, so let's add a couple
+        let userRecords = "insert into user (firstName, lastName, email, password, city, trainerLevel) values ?";
+        let recordValues = [
+          ["Jay", "Wang", "jaywang@bcit.ca", "123456", "Burnaby", "10"]
+        ];
+        await connection.query(userRecords, [recordValues]);
+    }
     console.log("Listening on port " + port + "!");
+
 }
 
 // RUN SERVER
---
+++ public/js/client.js
@@ -0,0 +1,15 @@
+// Client-Side
+
+function ready(callback) {
+    if (document.readyState != "loading") {
+        callback();
+        console.log("ready state is 'complete'");
+    } else {
+        document.addEventListener("DOMContentLoaded", callback);
+        console.log("Listener was invoked");
+    }
+}
+
+ready(function(){
+    console.log("Client Script Loaded.")
+})
