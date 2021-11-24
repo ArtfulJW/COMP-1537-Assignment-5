79af4eccd8abef5e742d1924fdb8596e969d89e4
Jay Wang <jwang550@my.bcit.ca>
2021-11-23 22:20:07 -0800

    General Setup

---
+++ PokeMart.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const session = require("express-session");
+const app = express();
+const fs = require("fs");
+const { JSDOM } = require('jsdom');
+
+// static path mappings
+app.use("/js", express.static("public/js"));
+app.use("/css", express.static("public/css"));
+app.use("/images", express.static("public/images"));
+app.use("/html", express.static("public/html"));
+
+app.get("/", function (req, res) {
+
+    doc = fs.readFileSync("./html/index.html", "utf-8");
+    res.send(doc);
+
+});
+
+async function init(){
+    console.log("Listening on port " + port + "!");
+}
+
+// RUN SERVER
+let port = 8000;
+app.listen(port, init);
